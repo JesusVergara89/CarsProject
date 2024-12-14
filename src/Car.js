6b6e12c31a349d608496e231269dbfa01196d650
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-12-14 13:15:27 -0600

    adding google font monserrart to the project, replace the render component test for the card component, creating a file for the forms

--- src/Car.js
+++ src/Car.js
@@ -1,12 +1,12 @@
-import TestRequestDataComponent from "./components/TestRequestDataComponent";
+import Card from "./components/Card";
 
 function Car() {
   return (
     <div className="w-screen h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">
         Cars
-      </h1> 
-      <TestRequestDataComponent/>     
+      </h1>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "../store/slices/crudSlice";
+
+const Card = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector(selectAllPosts);
+  const status = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [status, dispatch]);
+
+  let content;
+  if (status === "loading") {
+    content = <h2 className="text-slate-400 text-2xl font-bold">Loading...</h2>;
+  } else if (status === "success") {
+    content = posts.map((post, index) => (
+      <div
+        key={index}
+        className="w-1/2 h-1/2 sm:h-3/5 bg-white shadow-md rounded-lg p-4"
+      >
+        <h1 className="w-fit px-3 py-1 mb-2 border rounded-md text-2xl text-cyan-100 font-bold bg-slate-600">
+          {post.id}
+        </h1>
+        <img src={post.image} alt={post.title} className="w-full aspect-auto" />
+        <h2 className="text-xl font-bold">{post.date}</h2>
+        <p className="text-slate-400  text-sm sm:text-base ">
+          {post.description.slice(0, 270)}...
+        </p>
+      </div>
+    ));
+  } else if (status === "failed") {
+    content = <h2 className="text-slate-400 text-2xl font-bold">{error}</h2>;
+  }
+
+  return (
+    <div className="w-screen h-screen bg-gray-100 sm:flex sm:flex-wrap flex flex-col items-center justify-center gap-3">
+      {content}
+    </div>
+  );
+};
+
+export default Card;
