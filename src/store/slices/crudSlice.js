3add7863b28f4e85f91713a9aebc91ed848454d7
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-12-14 10:44:20 -0600

    configuring slice to request data to the API

--- src/store/slices/crudSlice.js
+++ src/store/slices/crudSlice.js
@@ -1,22 +1,38 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const URL = "https://script.google.com/macros/s/AKfycby0UcJnZfj6RWDVHeEFU3wiM2K_Lx8d9n-ZFU_T7BkOpQiRbGAYild9vNXPsMSC7xE3/exec?action=data";
+const URL =
+  "https://script.google.com/macros/s/AKfycby0UcJnZfj6RWDVHeEFU3wiM2K_Lx8d9n-ZFU_T7BkOpQiRbGAYild9vNXPsMSC7xE3/exec?action=data";
 
 const initialState = {
-    posts: [],
-    status: "idle",
-    error: null,
+  posts: [],
+  status: "idle",
+  error: null,
 };
 
-
+const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await axios.get(URL);
+  return response.data;
+});
 
 export const crudSlice = createSlice({
-    name: 'crud',
-    initialState: undefined,
-    reducers: {
-        
-    }
-})
+  name: "crud",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, payload) => {
+        state.posts = payload;
+        state.status = "success";
+      })
+      .addCase(fetchPosts.rejected, (state, error) => {
+        state.error = "Failed";
+        state.error = error.message;
+      });
+  },
+});
 
 export default crudSlice.reducer;
